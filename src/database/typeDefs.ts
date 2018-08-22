a1dac0bdf5e5a5303cd3cf054f558427af1045bb
Shane Johnson <shanej@ultravisual.co.uk>
2018-08-22 21:47:37 +0100

    initial commit

---
+++ src/database/typeDefs.ts
@@ -0,0 +1,16 @@
+export default `
+    type Query {
+        stats: [Stat]
+    },
+    type Stat {
+        name: String,
+        ID: Float,
+        token: String,
+        angle: Float,
+        temperature: Float,
+        battery: Float,
+        gravity: Float,
+        interval: Float,
+        RSSI: Float
+    }
+`
